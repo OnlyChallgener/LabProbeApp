aaa21120888b2cdc858d56f78e999cb7adedfa3a
OnlyChallgener <710646773@qq.com>
2026-07-22 19:15:27 +0800

    build: include router release text fixes [skip ci]

--- scripts/prepare_android_sources.py
+++ scripts/prepare_android_sources.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from apply_release_text_fixes import apply as apply_release_texts
 from apply_router_ui_fixes import patch_main, patch_router_ui
 from apply_wol_navigation_fix import apply as apply_wol_navigation
 
@@ -7,4 +8,5 @@ if __name__ == "__main__":
     patch_main()
     patch_router_ui()
     apply_wol_navigation()
+    apply_release_texts()
     print("Android source fixes prepared")
