d01893e3a9f475dcc6f52185555436e6763e9120
OnlyChallgener <710646773@qq.com>
2026-07-22 20:49:51 +0800

    build: include v0.10.13 source fixes [skip ci]

--- scripts/prepare_android_sources.py
+++ scripts/prepare_android_sources.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from apply_next_release_fixes import apply as apply_next_release
 from apply_release_text_fixes import apply as apply_release_texts
 from apply_router_ui_fixes import patch_main, patch_router_ui
 from apply_wol_navigation_fix import apply as apply_wol_navigation
@@ -9,4 +10,5 @@ if __name__ == "__main__":
     patch_router_ui()
     apply_wol_navigation()
     apply_release_texts()
+    apply_next_release()
     print("Android source fixes prepared")
