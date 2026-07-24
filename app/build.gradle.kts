dbcfd7c89a735ebf4d4d4d4b509e55c5b800fbfb
OnlyChallgener <710646773@qq.com>
2026-07-24 23:10:50 +0800

    chore: bump APP to build157 [skip ci]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         applicationId = "com.labprobe.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 156
+        versionCode = 157
         versionName = "0.10.15"
     }
 
