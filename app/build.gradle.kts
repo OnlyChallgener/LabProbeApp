73efab06b97b223ebc1393f6353d2064c5805afd
OnlyChallgener <710646773@qq.com>
2026-08-05 14:11:16 +0800

    chore: bump Android version to build176

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.labprobe.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 175
-        versionName = "0.10.33"
+        versionCode = 176
+        versionName = "0.10.34"
     }
 
     signingConfigs {
