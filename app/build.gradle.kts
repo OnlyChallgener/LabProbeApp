951a8f8b591b160031b9b64b53fee006d743c9d3
OnlyChallgener <710646773@qq.com>
2026-09-02 22:07:06 +0800

    chore(app): prepare 0.10.75 build230 test publish

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.labprobe.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 229
-        versionName = "0.10.74"
+        versionCode = 230
+        versionName = "0.10.75"
 
     }
 
