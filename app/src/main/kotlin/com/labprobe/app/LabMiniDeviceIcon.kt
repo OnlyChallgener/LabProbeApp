c851daa1d6ee93eb9c121c1da5a9960ecf699b1d
OnlyChallgener <710646773@qq.com>
2026-09-08 18:20:47 +0800

    fix(ui): remove redundant package import in LabMiniDeviceIcon

--- app/src/main/kotlin/com/labprobe/app/LabMiniDeviceIcon.kt
+++ app/src/main/kotlin/com/labprobe/app/LabMiniDeviceIcon.kt
@@ -15,7 +15,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.labprobe.app.ui.design.LabMaterialPolish
 
 @Composable
 fun LabMiniDeviceIcon(
